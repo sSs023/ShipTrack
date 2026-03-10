ea664b4ef7a0cb6b92639948697a5260de0b4dfc
Ulug'bek Karimov <ulugbekkarimov138@gmail.com>
2026-03-10 11:14:21 +0500

    fix: type errors

--- src/views/dashboard/ui/DashboardRecent.tsx
+++ src/views/dashboard/ui/DashboardRecent.tsx
@@ -1,6 +1,6 @@
-import { useShipments } from "../../../entities/shipment/model/use-shipments";
+// import { useShipments } from "../../../entities/shipment/model/use-shipments";
 
 export default function DashboardRecent() {
-  const query = useShipments();
+  // const query = useShipments();
   return <div>DashboardRecent</div>;
 }
